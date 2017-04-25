233d1b7739270e11cfe1ea7218ab55b0cf9f069b
adm_yqin <adm_yqin@AD.Brown.Edu>
2017-04-25 10:01:36 -0400

    Add BGInfo settings and pull settings

---
+++ LCM_HttpPull.ps1
@@ -0,0 +1,23 @@
+﻿[DSCLocalConfigurationManager()]
+Configuration LCM_HttpPull {
+
+    param (
+        [Parameter(Mandatory=$true)][String[]]$ComputerName,
+        [Parameter(Mandatory=$true)][String]$guid
+    )
+
+    Node $ComputerName {
+
+        Settings {
+            AllowModuleOverwrite = $true
+            ConfigurationMode = 'ApplyAndAutoCorrect'
+            RefreshMode = 'Pull'
+            ConfigurationID = $guid
+        }
+
+        ConfigurationRepositoryWeb DSCHTTP {
+            ServerURL = "http://DWIN2016DSCCIT.ad.brown.edu:8080/PSDSCPullServer.svc"
+            AllowUnsecureConnection = $true
+        }
+    }
+}
